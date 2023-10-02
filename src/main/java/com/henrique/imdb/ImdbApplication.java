3d445e1518ca5e3c8a796b4ee0973d73240bd7cf
Henrique Romano <henriquercorreia@gmail.com>
2023-10-01 23:19:54 -0300

    Hello World!

---
+++ src/main/java/com/henrique/imdb/ImdbApplication.java
@@ -0,0 +1,14 @@
+package com.henrique.imdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
